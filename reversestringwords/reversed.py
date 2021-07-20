6676055c990fc43eec27c27448113368ee592e9d
Fue Moua <MadisonLifeAgent@gmail.com>
2021-07-20 14:31:28 -0500

    finished reversed.py

--- reversestringwords/reversed.py
+++ reversestringwords/reversed.py
@@ -18,7 +18,7 @@ class Phrase:
         self.original_phrase = ""
         self.words_list = []
         self.new_phrase = ""
-        self.reversed_words_list = []
+        self.reverse_words_list = []
 
     #set methods
     # get phrase method
@@ -29,11 +29,39 @@ class Phrase:
     def split_up_phrase(self):
         self.words_list = self.original_phrase.split()
 
+    # reverse the words in the array
+    def reverse_words(self):
+        for i in self.words_list:
+            #store the word to be appended into new array
+            self.reverse_words_list.append(i[::-1])
+
+    # take new array and make a new phrase with reverse words but words in orginal order
+    def new_user_phrase(self):
+        for x in self.reverse_words_list:
+            #use this to end the new string without an extra space at the end
+            if self.reverse_words_list.index(x) == len(self.reverse_words_list):
+                self.new_phrase += x
+            #add the reverse word to the new phrase
+            else:
+                self.new_phrase += x + " "
+
 #instantiate methods
 user_phrase = Phrase()
 
 #get user phrase
 user_phrase.get_phrase()
 
-#debugging
+#split up the phrase into an list of words
+user_phrase.split_up_phrase()
+
+#reverse each word in the new list
+user_phrase.reverse_words()
+
+#get the new reversed word phrase
+user_phrase.new_user_phrase()
+
+#print the result of each method call
 print(user_phrase.original_phrase)
+print(user_phrase.words_list)
+print(user_phrase.reverse_words_list)
+print(user_phrase.new_phrase)
